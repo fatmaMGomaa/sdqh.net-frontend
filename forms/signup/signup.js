70666e98cdb663007e6b41413c45281176cb943a
fatmaMGomaa <fatma-mohamed@feps.edu.eg>
2019-10-20 16:54:16 +0200

    edit user profile handling in signup page

--- forms/signup/signup.js
+++ forms/signup/signup.js
@@ -1,51 +1,96 @@
-let data, image;
-const form = document.getElementById('signup')
-const imageFile = document.getElementById('image')
-imageFile.addEventListener("change", (e)=>{
-    image = e.target.files[0]
-})
+let data;
+const actionType = getLocalStorageItem("actionType");
+removeLocalStorageItem("actionType");
+const form = document.getElementById('signup');
+const formButton = document.getElementById('add-edit-btn')
+const firstName = document.getElementById('firstName')
+const lastName = document.getElementById('lastName')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const gender = document.getElementById('gender')
+const birthDate = document.getElementById('birthdate')
+const image = document.getElementById('image')
+// const imageFile = document.getElementById('image')
+// imageFile.addEventListener("change", (e)=>{
+//     image = e.target.files[0]
+// })
+
+if (actionType === "edit") {
+    const newPassword = document.getElementById('newPassword');
+    const newPasswordDiv = document.getElementById('addNewPassword');
+    newPasswordDiv.style.display="block"
+    formButton.textContent = "تعديل";
+    firstName.value = user.firstName;
+    lastName.value = user.lastName;
+    email.value = user.email;
+    gender.value = user.gender;
+    birthDate.value = user.birthDate;
+    image.value = user.image;
+}
 
 form.addEventListener("submit",(e)=>{
     e.preventDefault();
-    const firstName = document.getElementById('firstName').value
-    const lastName = document.getElementById('lastName').value
-    const email = document.getElementById('email').value
-    const password = document.getElementById('password').value
-    const gender = document.getElementById('gender').value
-    const birthDate = document.getElementById('birthdate').value
-    const image = document.getElementById('image').value
-    
     let data = new FormData();
-    data.append("firstName", firstName);
-    data.append("lastName", lastName);
-    data.append("email", email);
-    data.append("password", password);
-    data.append("birthDate", birthDate);
-    data.append("gender", gender);
-    data.append("image", image);
+    data.append("firstName", firstName.value);
+    data.append("lastName", lastName.value);
+    data.append("email", email.value);
+    data.append("password", password.value);
+    data.append("birthDate", birthDate.value);
+    data.append("gender", gender.value);
+    data.append("image", image.value);
     // if (image) {
     //     data.append("file", image);
     // }
-
-    axios
-        .post(backendURL+"signup", data, {
-            headers: {
-                accept: "application/json",
-                "Accept-Language": "en-US,en;q=0.8",
-                "Content-Type": `multipart/form-data; boundary=${data._boundary}`
-            }
-        })
-        .then(function (response) {
-            data = response.data;
-            localStorage.clear();
-            saveToLocalStorage("token", data.token)
-            saveToLocalStorage("user", data.user)
-            saveToLocalStorage("userId", data.user.id)
-            saveToLocalStorage("userProfile", data.user)
-            window.location.replace(baseURL + "/landingPage/landing.html");
-        })
-        .catch(function (error) {
-            console.log(error);
-            alert(error.response.data.message)
-        });
+    if (actionType === "edit") {
+        data.append("newPassword", newPassword.value);
+        axios
+            .put(`${backendURL}editUser`, data, {
+                headers: {
+                    accept: "application/json",
+                    "Accept-Language": "en-US,en;q=0.8",
+                    "Content-Type": `multipart/form-data; boundary=${data._boundary}`,
+                    Authorization: `bearer ${token}`
+                }
+            })
+            .then(function (response) {
+                data = response.data;
+                saveToLocalStorage("token", data.token)
+                saveToLocalStorage("user", data.user)
+                saveToLocalStorage("userId", data.user.id)
+                saveToLocalStorage("userProfile", data.user)
+                window.location.replace(baseURL + "/userProfile/userProfile.html");
+                alert("your profile was updated successfully")
+            })
+            .catch(function (error) {
+                console.log(error);
+                if (error.response) {
+                    alert(error.response.data.message)
+                } else {
+                    alert("something went wrong")
+                    window.location.replace(baseURL + "/userProfile/userProfile.html");
+                }
+            });
+    }else {
+        axios
+            .post(backendURL + "signup", data, {
+                headers: {
+                    accept: "application/json",
+                    "Accept-Language": "en-US,en;q=0.8",
+                    "Content-Type": `multipart/form-data; boundary=${data._boundary}`
+                }
+            })
+            .then(function (response) {
+                data = response.data;
+                localStorage.clear();
+                saveToLocalStorage("token", data.token)
+                saveToLocalStorage("user", data.user)
+                saveToLocalStorage("userId", data.user.id)
+                saveToLocalStorage("userProfile", data.user)
+                window.location.replace(baseURL + "/landingPage/landing.html");
+            })
+            .catch(function (error) {
+                console.log(error);
+                alert(error.response.data.message)
+            });
+    }
 })
